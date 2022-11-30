31e5fefa228d82a9593b725e064ef832b7c10c5d
Heber Junior <junio.heber@gmail.com>
2022-11-30 13:46:55 +0000

    finalizing reader class

--- src/eirvidmovierentalsystem/FileReader.java
+++ src/eirvidmovierentalsystem/FileReader.java
@@ -3,7 +3,20 @@ package eirvidmovierentalsystem;
 public class FileReader {
     public class FileReader extends Menu {
 
+// Read the content from file
+try(FileReader fileReader = new FileReader(absolutePath)) {
 
+    int ch = fileReader.read();
+    while(ch != -1) {
+        System.out.print((char)ch);
+        fileReader.close();
+    }
+    
+} catch (FileNotFoundException e) {
+    // Exception handling
+} catch (IOException e) {
+    // Exception handling
+}
 
     }
 }
